db5f849aead946bc46af48dbf0c1f6f150e42f90
Jaidjen <44955827+Jaidjen@users.noreply.github.com>
2019-05-26 22:27:07 -0800

    Create a_new_file.py

---
+++ a_new_file.py
@@ -0,0 +1,28 @@
+def repeat_line(n):
+     n()
+     n()
+
+def repeat_lines(n):
+    repeat_line(n)
+    repeat_line(n)
+
+def print_bar():
+    print('+----+----+'),
+
+
+def print_stick():
+    print('|    |    |'),
+
+def print_rows():
+    print_bar()
+    repeat_lines(print_stick)
+
+def print_gridlines():
+    repeat_line(print_rows)
+    print_bar()
+
+print_gridlines()
+
+
+
+
